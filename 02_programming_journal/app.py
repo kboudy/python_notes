05c7fa265e4ba8beb824bc20dedc6b029aff87cc
Keith Boudreau <kboudy@gmail.com>
2020-10-03 17:49:36 -0400

    programming journal

--- 02_programming_journal/app.py
+++ 02_programming_journal/app.py
@@ -8,10 +8,12 @@ menu = """Please select one of the following options:
 Your selection: """
 welcome = "Welcome to the programming diary!"
 
+
 def prompt_new_entry():
     entry_content = input("What have you learned today? ")
     entry_date = input("Enter the date: ")
     add_entry(entry_content, entry_date)
+
 
 def view_entries(entries):
     for entry in entries:
@@ -20,7 +22,8 @@ def view_entries(entries):
 
 print(welcome)
 
-while (user_input := input(menu)) != "3":  # := is the Python 3.8 walrus operator
+# := is the Python 3.8 walrus operator
+while (user_input := input(menu)) != "3":
     if user_input == "1":
         prompt_new_entry()
     elif user_input == "2":
